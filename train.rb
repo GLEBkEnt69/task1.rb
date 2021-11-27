edd09d89bec8bcd788403cfcbf7f8d2fcd1625f5
GLEBkEnt69 <85952092+GLEBkEnt69@users.noreply.github.com>
2021-11-27 12:03:04 +0000

    m

---
+++ train.rb
@@ -0,0 +1,13 @@
+class Train
+    def initialize(number, type, van_count)
+        @number = number
+        @type = type
+        @van_count = van_count
+        
+    end
+
+    def  aaa
+        puts @number
+        
+    end
+end
